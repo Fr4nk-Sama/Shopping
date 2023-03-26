0f79ad56a59e9e32a10e9e3b9c760fce99dfbe0e
Fr4nk-Sama <l19111772@itcj.edu.mx>
2023-03-25 23:26:40 -0600

    Respald

--- Shopping/Data/Entities/Category.cs
+++ Shopping/Data/Entities/Category.cs
@@ -5,9 +5,16 @@ namespace Shopping.Data.Entities
     public class Category
     {
         public int Id { get; set; }
-        [Display(Name = "Categoria")]
-        [MaxLength(50, ErrorMessage = "El campo {0} debe de tener maximo {1} caracteres.")]
+
+        [Display(Name = "Categoría")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
         [Required(ErrorMessage = "El campo {0} es obligatorio.")]
         public string Name { get; set; }
+
+        public ICollection<ProductCategory> ProductCategories { get; set; }
+
+        [Display(Name = "# Productos")]
+        public int ProductsNumber => ProductCategories == null ? 0 : ProductCategories.Count();
+
     }
 }
---
+++ Shopping/Data/Entities/ProductImage.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shopping.Data.Entities
+{
+    public class ProductImage
+    {
+        public int Id { get; set; }
+
+        public Product Product { get; set; }
+
+        [Display(Name = "Foto")]
+        public Guid ImageId { get; set; }
+
+        //TODO: Pending to change to the correct path
+        [Display(Name = "Foto")]
+        public string ImageFullPath => ImageId == Guid.Empty
+            ? $"https://localhost:7013/images/No_image.png"
+            : $"https://shopping8.blob.core.windows.net/products/{ImageId}";
+
+    }
+}
